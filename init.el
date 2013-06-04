ad2d62950d64ab9e3c8ad97340a8c7bd66138aba
Nathan Houle <nathan@nathanhoule.com>
2013-06-04 11:32:31 -0700

    Switch from starter kit to emacs.d from scratch for learning good

--- init.el
+++ init.el
@@ -1,18 +1,95 @@
-;;; init.el --- Where all the magic begins
+;;; init.el -- Where the Emagic begins
 ;;
-;; Part of the Emacs Starter Kit
+;;; Commentary:
 ;;
-;; This is the first thing to get loaded.
+;; This is the entry point for all things Emacs.  This file gets loaded first.
 ;;
+;;; Code:
 
-;; load the starter kit from the `after-init-hook' so all packages are loaded
-(add-hook 'after-init-hook
- `(lambda ()
-    ;; remember this directory
-    (setq starter-kit-dir
-          ,(file-name-directory (or load-file-name (buffer-file-name))))
-    ;; load up the starter kit
-    (require 'org)
-    (org-babel-load-file (expand-file-name "starter-kit.org" starter-kit-dir))))
+;; ----------
+;; Load Path
+;; ----------
+; Add a path to Emacs' load path.
+(defun add-to-loadpath (&rest dirs)
+  (dolist (dir dirs load-path)
+  (add-to-list 'load-path (expand-file-name dir) nil #'string=)))
+
+(add-to-loadpath "~/.emacs.d/config")
+
+
+;; ----------
+;; OS-Specific Settings
+;; ----------
+; Determine if we're on a Mac
+(setq is-mac (equal system-type 'darwin))
+
+
+;; ----------
+;; Packages
+;; ----------
+(setq package-archives
+      '(("gnu"         . "http://elpa.gnu.org/packages/")
+        ("original"    . "http://tromey.com/elpa/")
+        ("org"         . "http://orgmode.org/elpa/")
+        ("marmalade"   . "http://marmalade-repo.org/packages/")))
+(package-initialize)
+
+(defvar ndhoule-packages
+  '(clojure-mode
+    evil
+    evil-leader
+    evil-nerd-commenter
+    flycheck
+    fill-column-indicator
+    magit
+    paredit
+    rainbow-delimiters
+    smooth-scrolling
+
+    ; Modes
+    auto-indent-mode
+    coffee-mode
+    css-mode
+    csv-mode
+    doc-mode
+    gitconfig-mode
+    gitignore-mode
+    haml-mode
+    jade-mode
+    json-mode
+    less-css-mode
+    markdown-mode
+    mustache-mode
+    nginx-mode
+    python-mode
+    requirejs-mode
+    ruby-mode
+    sass-mode
+
+    ; Themes
+    monokai-theme)
+    "A list of packages that should be present at launch.")
+
+;; Check for new packages and install them if missing
+(unless package-archive-contents
+  (package-refresh-contents))
+(dolist (package ndhoule-packages)
+  (when (not (package-installed-p package))
+    (package-install package)))
+
+
+;; ----------
+;; Config Files
+;; ----------
+; Provide a list of config files and include them
+(defvar ndhoule/config
+  '(ndhoule-evil
+    ndhoule-color
+    ndhoule-flycheck
+    ndhoule-general
+    ndhoule-linenumbers
+    ndhoule-whitespace))
+(mapc #'require ndhoule/config)
+
 
 ;;; init.el ends here
