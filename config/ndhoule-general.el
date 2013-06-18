bdddd6b89f9825eb53f9229e65b531eda63c96da
Nathan Houle <nathan@nathanhoule.com>
2013-06-18 13:39:48 -0700

    Alter loadpath

--- config/ndhoule-general.el
+++ config/ndhoule-general.el
@@ -30,11 +30,13 @@
 ; Use rainbow parens
 (global-rainbow-delimiters-mode)
 
-; Save backup files in system temporary directory (e.g. /tmp)
-(setq backup-directory-alist
-            `((".*" . ,temporary-file-directory)))
-(setq auto-save-file-name-transforms
-            `((".*" ,temporary-file-directory t)))
+;; Put autosave files (ie #foo#) and backup files (ie foo~) in ~/.emacs.d/.
+(custom-set-variables
+ '(auto-save-file-name-transforms '((".*" "~/.emacs.d/autosaves/\\1" t)))
+ '(backup-directory-alist '((".*" . "~/.emacs.d/backups/"))))
+
+;; create the autosave dir if necessary, since emacs won't.
+(make-directory "~/.emacs.d/autosaves/" t)
 
 ; Prevent jumpy scrolling
 (require 'smooth-scrolling)
