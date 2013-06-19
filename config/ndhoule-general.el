33d62dec1c63dc6a9dff80b985f67568b24f1610
Nathan Houle <nathan@nathanhoule.com>
2013-06-18 23:52:07 -0700

    Add relative line numbering

--- config/ndhoule-general.el
+++ config/ndhoule-general.el
@@ -13,21 +13,21 @@
 ;; Don't create lockfiles
 (setq create-lockfiles nil)
 
-; Turn off visual and audio bells
+;; Turn off visual and audio bells
 (setq ring-bell-function 'ignore)
 
-; Disable top menu bar
+;; Disable top menu bar
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 
-; Automatically insert closing character of paired characters, like () and ""
+;; Automatically insert closing character of paired characters, like () and ""
 (paredit-mode 1)
 
-; Show matching parens/braces and disable delay
+;; Show matching parens/braces and disable delay
 (show-paren-mode 1)
 (setq show-paren-delay 0)
 
-; Use rainbow parens
+;; Use rainbow parens
 (global-rainbow-delimiters-mode)
 
 ;; Put autosave files (ie #foo#) and backup files (ie foo~) in ~/.emacs.d/.
@@ -38,26 +38,27 @@
 ;; create the autosave dir if necessary, since emacs won't.
 (make-directory "~/.emacs.d/autosaves/" t)
 
-; Prevent jumpy scrolling
+;; Prevent jumpy scrolling
 (require 'smooth-scrolling)
 (eval-after-load 'smooth-scrolling
   '(progn
      (setq smooth-scroll-margin 5)
      nil))
 
-; Set a default browser
+;; Set a default browser
 (setq browse-url-browser-function 'browse-default-macosx-browser)
 
-; This isn't 1980, thanks
+;; This isn't 1980, thanks
 (setq sentence-end-double-space nil)
 
-; Disable warnings about symlink
+;; Disable warnings about symlink
 (setq find-file-visit-truename t)
 
-; Highlight the current line
+;; Highlight the current line
 (global-hl-line-mode 1)
+(set-face-background 'hl-line "#303030")
 
-; Enable 80-character warning
+;; Enable 80-character warning
 (setq-default fill-column 79)
 (require 'fill-column-indicator)
 (setq fci-rule-width 1)
