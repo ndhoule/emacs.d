81efbcdded38c94acc6b649b08cd6bc412ad099a
Nathan Houle <nathan@nathanhoule.com>
2013-06-18 23:53:55 -0700

    Color changes

--- config/ndhoule-color.el
+++ config/ndhoule-color.el
@@ -1,13 +1,19 @@
 ;;; ndhoule-color.el
 
-; Colorful parentheticals
-; (require 'rainbow-delimiters)
+;; Colorful parentheticals
+;; (require 'rainbow-delimiters)
 
-; Set default font
+;; Set default font
 (set-default-font "DejaVu Sans Mono-12")
 
-; Set theme
-; (load-theme 'monokai t)
+;; Set theme
+(load-theme 'monokai t)
+
+;; Set line highlight color
+(set-face-background 'hl-line "#303030")
+
+;; Make area select color nicer
+(set-face-background 'region "#595959")
 
 (provide 'ndhoule-color)
 ;;; ndhoule-color.el ends here
