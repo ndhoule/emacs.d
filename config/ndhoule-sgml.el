e0c2fbcf4e9d1eb455c29819c93d443db07be133
Nathan Houle <nathan@nathanhoule.com>
2013-09-07 16:39:52 -0700

    Switch from zencoding-mode to emmet

--- config/ndhoule-sgml.el
+++ config/ndhoule-sgml.el
@@ -10,13 +10,19 @@
 (require-package 'jade-mode)
 (require-package 'mustache-mode)
 (require-package 'sws-mode)
-(require-package 'zencoding-mode)
+(require-package 'emmet-mode)
 
 (add-to-list 'auto-mode-alist '("\\.styl$" . sws-mode))
 (add-to-list 'auto-mode-alist '("\\.jade$" . jade-mode))
 
-;; Enable Zencoding mode in all SGML languages
-(add-hook 'sgml-mode-hook 'zencoding-mode)
+;; Enable Emmet mode in compatible modes
+(add-hook 'sgml-mode-hook 'emmet-mode)
+(add-hook 'css-mode-hook 'emmet-mode)
+(add-hook 'less-mode-hook 'emmet-mode)
+(add-hook 'sass-mode-hook 'emmet-mode)
+(add-hook 'handlebars-mode-hook 'emmet-mode)
+
+(add-hook 'emmet-mode-hook (lambda () (setq emmet-indentation 2)))
 
 (provide 'ndhoule-sgml)
 ;;; ndhoule-sgml.el ends here
