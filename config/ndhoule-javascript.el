731ba5282d5bc8def34786ce6bf4855cd4460c09
Nathan Houle <nathan@nathanhoule.com>
2013-10-17 11:55:20 -0700

    Switch to major js-mode with js2-minor-mode

--- config/ndhoule-javascript.el
+++ config/ndhoule-javascript.el
@@ -8,7 +8,7 @@
 
 
 (require-package 'coffee-mode)
-(require-package 'js3-mode)
+(require-package 'js2-mode)
 (require-package 'json-mode)
 (require-package 'requirejs-mode)
 
@@ -25,35 +25,20 @@
         (setq tern-ac-on-dot nil)
         (tern-ac-setup))))
 
-;; Ensure we use js3-mode rather than the built-in (outdated) javascript-mode
-(add-to-list 'auto-mode-alist '("\\.js$" . js3-mode))
+;; Ensure we use js2-mode rather than the built-in (outdated) javascript-mode
+(add-to-list 'auto-mode-alist '("\\.js$" . js-mode))
 
-(defun ndhoule/js3-mode-hook ()
-  "Default settings for JS3 mode."
+(defun ndhoule/js-mode-hook ()
+  "Hook for setting `js-mode' defaults and enabling `js2-minor-mode'."
 
-  ;; Disable a lot of (weird,) fancy indentation
-  (setq js3-consistent-level-indent-inner-bracket t)
+  (js2-minor-mode 1)
 
   ;; Set sane indentation defaults
-  (setq js3-indent-level preferred-javascript-indent-level)
-  (setq js3-basic-offset preferred-javascript-indent-level)
-  (setq js3-bounce-indent-p t)
-  (setq js3-auto-indent-p t)
-  (setq js3-enter-indents-newline t)
-  (setq js3-indent-on-enter-key t)
-  (setq js3-pretty-multiline-decl-indentation-p t)
+  (setq js-indent-level preferred-javascript-indent-level)
+  (setq js2-pretty-multiline-declarations t)
 
-  ;; Don't require spaces between `function()` -- default is `function ()`
-  (setq js3-mode-squeeze-spaces t)
-
-  ;; Set moar good defaults
-  (setq js3-mode-must-byte-compile nil)
-  (setq js3-auto-insert-catch-block t)
-  (setq js3-highlight-external-variables nil)
-  (setq js3-highlight-level 3)
-  (setq js3-pretty-multiline-declarations t)
-  (setq js3-mirror-mode nil)
-  (setq js3-cleanup-whitespace t)
+  (setq js2-highlight-external-variables nil)
+  (setq js2-highlight-level 3)
 
   ;; Enable Tern
   (tern-mode t)
@@ -64,13 +49,11 @@
   ;; Highlight keywords like TODO, etc.
   (lambda ()
     (hs-minor-mode 1)
-    (define-key js3-mode-map "\C-ci" 'js-doc-insert-function-doc)
-    (define-key js3-mode-map "@" 'js-doc-insert-tag)
     (font-lock-add-keywords nil
                             '(("\\<\\(XXX\\|FIXME\\|TODO\\|BUG\\):" 1 font-lock-warning-face t)))))
 
 ;; Enterprise, go!
-(add-hook 'js3-mode-hook (lambda () (ndhoule/js3-mode-hook)))
+(add-hook 'js-mode-hook (lambda () (ndhoule/js-mode-hook)))
 
 (provide 'ndhoule-javascript)
 ;;; ndhoule-javascript.el ends here
