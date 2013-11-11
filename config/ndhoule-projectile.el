ca7c3d4622f57f1c95af3e85d531a2cb6e83909c
Nathan Houle <nathan@nathanhoule.com>
2013-11-11 09:14:01 -0800

    Add description for projectile config file

--- config/ndhoule-projectile.el
+++ config/ndhoule-projectile.el
@@ -1,60 +1,14 @@
-;;; ndhoule-ffip.el --- Configuration for find-file-in-project
+;;; ndhoule-projectile.el --- Configuration for projectile.el
 
 ;;; Commentary:
 ;;
-;; Some of the defaults are crazy.
+;; Settings for Projectile go in here.
 
 ;;; Code:
 
-(require-package 'find-file-in-project)
+(require-package 'projectile)
 
-;; No need to be stingy
-(setq ffip-limit 4096)
+(projectile-global-mode)
 
-;; Helper methods to create local settings
-(defun ffip--create-exclude-find-options (names)
-  "Generate a regex we'll pass to ffip-find-options."
-  (mapconcat (lambda (name)
-               (concat "-not -regex \".*" name ".*\"")) names " "))
-
-(defun ffip-local-excludes (&rest names)
-  "Given a set of names, will exclude results with those names in the path.
-
-Example:
-(ffip-local-excludes \"target\" \"overlays\")"
-  (set (make-local-variable 'ffip-find-options)
-       (ffip--create-exclude-find-options names)))
-
-(defun ffip-local-patterns (&rest patterns)
-  "An exhaustive list of file name patterns to look for.
-
-Example:
-(ffip-local-patterns \"*.js\" \"*.jsp\" \"*.css\")"
-  (set (make-local-variable 'ffip-patterns) patterns))
-
-
-;; Function to create new functions that look for a specific pattern
-(defun ffip-create-pattern-file-finder (&rest patterns)
-  (lexical-let ((patterns patterns))
-    (lambda ()
-      (interactive)
-      (let ((ffip-patterns patterns))
-        (find-file-in-project)))))
-
-;; Default excludes - override with ffip-local-excludes
-(setq ffip-find-options
-      (ffip--create-exclude-find-options
-       '(".git"
-         ".meteor"
-         ".svn"
-         "flycheck-"
-         "elpa"
-         "node_modules"
-         "bower_components"
-         "rtd"
-         ".tmp"
-         "coverage"
-         ".min.js")))
-
-(provide 'ndhoule-ffip)
-;;; ndhoule-ffip.el ends here
+(provide 'ndhoule-projectile)
+;;; ndhoule-projectile.el ends here
