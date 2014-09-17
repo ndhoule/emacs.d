c815900844cb62274f1d65d8620655d3e72b89b7
Nathan Houle <nathan@nathanhoule.com>
2014-09-17 16:39:20 -0700

    Fix incorrect autosave path

--- config/ndhoule-general.el
+++ config/ndhoule-general.el
@@ -6,25 +6,16 @@
 
 ;;; Code:
 
-;; Disable splash screen
 (setq inhibit-startup-message t)
-
-;; Turn off visual and audio bells
 (setq ring-bell-function 'ignore)
-
-;; Don't create lockfiles
 (setq create-lockfiles nil)
-
-;; Put autosave files (ie #foo#) and backup files (ie foo~) in ~/.emacs.d/.
-(make-directory "~/.emacs.d/autosaves/" t)
-(custom-set-variables
- `(auto-save-file-name-transforms `((".*" ~(expand-file-name "autosaves/\\1" user-emacs-directory) t))))
-
-;; Disable warnings about editing symlinked files
 (setq find-file-visit-truename t)
-
-;; Autoreload files on change
 (global-auto-revert-mode t)
+
+;; Change autosave directory
+(let ((autosave-dir (expand-file-name "autosaves/\\1" user-emacs-directory)))
+  (make-directory autosave-dir t)
+  (setq auto-save-file-name-transforms `((".*" ,autosave-dir))))
 
 ;; Prevent scrolling from jumping half-pages at a time
 (require-package 'smooth-scrolling)
