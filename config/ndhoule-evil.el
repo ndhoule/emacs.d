94d3afd9c1acb057e76164c51b7fc8abafbfb269
Nathan Houle <nathan@nathanhoule.com>
2014-09-17 10:45:36 -0700

    Clean up evil-mode settings

--- config/ndhoule-evil.el
+++ config/ndhoule-evil.el
@@ -12,33 +12,23 @@
 (require-package 'evil-paredit)
 (require-package 'evil-surround)
 
-;; Enable Evil (vim-like) mode
 (evil-mode 1)
-
-;; Enable evil-leader everywhere
-(global-evil-leader-mode)
-
 (setq evil-default-cursor t)
-
-; Enable evil-surround
+(global-evil-leader-mode)
 (global-evil-surround-mode 1)
-
-;; Set cursor to red bar in insert mode
-(setq evil-insert-state-cursor '("red" hbar))
-
-;; Use space to enable ace jump mode
-(define-key evil-normal-state-map (kbd "SPC") 'ace-jump-mode)
-
-;; Set evil's mapleader key to comma
 (evil-leader/set-leader ",")
 
-;; Use ; as : for glorious RSI prevention
+;; Use ; as :
 (define-key evil-normal-state-map (kbd ";") 'evil-ex)
 (define-key evil-visual-state-map (kbd ";") 'evil-ex)
 (define-key evil-motion-state-map (kbd ";") 'evil-ex)
 
 ;; Shortcut for wrapping paragraphs at line limit
 (define-key evil-normal-state-map (kbd "Q") (kbd "gqap"))
+
+;;
+;; Keybindings
+;;
 
 (evil-leader/set-key
   "ra"      'rainbow-delimiters-mode
@@ -72,7 +62,14 @@
   "n"       'sr-speedbar-toggle
   "o"       'open-file-with)
 
-;; Add evil bindings to Magit
+
+;; Use space to enable ace jump mode
+(define-key evil-normal-state-map (kbd "SPC") 'ace-jump-mode)
+
+;;
+;; magit-mode keybindings
+;;
+
 (evil-add-hjkl-bindings *bc-menu-mode-map* 'emacs)
 (evil-add-hjkl-bindings magit-mode-map 'emacs)
 (evil-add-hjkl-bindings magit-diff-mode-map 'emacs)
@@ -89,85 +86,26 @@
   "K" 'magit-discard-item
   "L" 'magit-key-mode-popup-logging)
 
-;; Ibuffer
+;;
+;; ibuffer-mode keybindings
+;;
+
 (eval-after-load 'ibuffer
   '(progn
+     ;;; TODO: Does this actually need to exist?
+     ;; (set-keymap-parent
+     ;;  (evil-get-auxiliary-keymap ibuffer-mode-map 'normal t)
+     ;;  (assq-delete-all 'menu-bar (copy-keymap ibuffer-mode-map)))
      (evil-make-overriding-map ibuffer-mode-map 'normal t)
      (evil-define-key 'normal ibuffer-mode-map
-       "j" 'evil-next-line
-       "k" 'evil-previous-line
-       "RET" 'ibuffer-visit-buffer)))
+       ;; "j" 'evil-next-line
+       ;; "k" 'evil-previous-line
+       "j" 'ibuffer-forward-line
+       "k" 'ibuffer-backward-line
+       ;; "J" 'ibuffer-jump-to-buffer
+       "RET" 'ibuffer-visit-buffer
+       (kbd "DEL") 'ibuffer-unmark-backward)))
 
-(eval-after-load 'ibuffer
-  '(progn
-     ;; Use standard ibuffer bindings as a base
-     (set-keymap-parent
-      (evil-get-auxiliary-keymap ibuffer-mode-map 'normal t)
-      (assq-delete-all 'menu-bar (copy-keymap ibuffer-mode-map)))
-     (evil-define-key 'normal ibuffer-mode-map "j" 'ibuffer-forward-line)
-     (evil-define-key 'normal ibuffer-mode-map "k" 'ibuffer-backward-line)
-     (evil-define-key 'normal ibuffer-mode-map "J" 'ibuffer-jump-to-buffer) ; "j"
-     (evil-define-key 'normal ibuffer-mode-map (kbd "DEL") 'ibuffer-unmark-backward)))
-
-;; AceJump is a nice addition to evil's standard motions. The following
-;; definitions are necessary to define evil motions for ace-jump-mode
-;; (version 2).
-(defmacro evil-enclose-ace-jump (&rest body)
-  `(let ((old-mark (mark))
-         (ace-jump-mode-scope 'window))
-     (remove-hook 'pre-command-hook #'evil-visual-pre-command t)
-     (remove-hook 'post-command-hook #'evil-visual-post-command t)
-     (unwind-protect
-         (progn
-           ,@body
-           (recursive-edit))
-       (if (evil-visual-state-p)
-           (progn
-             (add-hook 'pre-command-hook #'evil-visual-pre-command nil t)
-             (add-hook 'post-command-hook #'evil-visual-post-command nil t)
-             (set-mark old-mark))
-         (push-mark old-mark)))))
-
-(evil-define-motion evil-ace-jump-char-mode (count)
-  :type exclusive
-  (evil-enclose-ace-jump
-   (ace-jump-mode 5)))
-
-(evil-define-motion evil-ace-jump-line-mode (count)
-  :type line
-  (evil-enclose-ace-jump
-   (ace-jump-mode 9)))
-
-(evil-define-motion evil-ace-jump-word-mode (count)
-  :type exclusive
-  (evil-enclose-ace-jump
-   (ace-jump-mode 1)))
-
-(evil-define-motion evil-ace-jump-char-to-mode (count)
-  :type exclusive
-  (evil-enclose-ace-jump
-   (ace-jump-mode 5)
-   (forward-char -1)))
-
-(add-hook 'ace-jump-mode-end-hook 'exit-recursive-edit)
-
-;; Ace Keybindings
-(define-key evil-motion-state-map (kbd "SPC") #'evil-ace-jump-char-mode)
-(define-key evil-motion-state-map (kbd "C-SPC") #'evil-ace-jump-word-mode)
-
-(define-key evil-operator-state-map (kbd "SPC") #'evil-ace-jump-char-mode)      ; similar to f
-(define-key evil-operator-state-map (kbd "C-SPC") #'evil-ace-jump-char-to-mode) ; similar to t
-(define-key evil-operator-state-map (kbd "M-SPC") #'evil-ace-jump-word-mode)
-
-;; different jumps for different visual modes
-(defadvice evil-visual-line (before spc-for-line-jump activate)
-  (define-key evil-motion-state-map (kbd "SPC") #'evil-ace-jump-line-mode))
-
-(defadvice evil-visual-char (before spc-for-char-jump activate)
-  (define-key evil-motion-state-map (kbd "SPC") #'evil-ace-jump-char-mode))
-
-(defadvice evil-visual-block (before spc-for-char-jump activate)
-  (define-key evil-motion-state-map (kbd "SPC") #'evil-ace-jump-char-mode))
 
 (provide 'ndhoule-evil)
 ;;; ndhoule-evil.el ends here
