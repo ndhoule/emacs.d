078d1416bffe7e20212b1b6a98cdac9f64cbe853
Nathan Houle <nathan@nathanhoule.com>
2014-09-17 11:45:07 -0700

    Break git commit lines at 72 chars

--- config/ndhoule-git.el
+++ config/ndhoule-git.el
@@ -12,5 +12,12 @@
 (require-package 'gitignore-mode)
 (require-package 'github-browse-file)
 
+;; Break commit lines at 72 characters
+(add-hook 'magit-log-edit-mode-hook
+          (lambda ()
+            (set-fill-column 72)
+            (auto-fill-mode t)))
+
+
 (provide 'ndhoule-git)
 ;;; ndhoule-git.el ends here
