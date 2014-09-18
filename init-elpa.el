2042d99fd6f4324fc5b760caa0b7e8afb0cda79a
Nathan Houle <nathan@nathanhoule.com>
2014-09-17 18:11:23 -0700

    Update init-elpa to use melpa-stable, other improvements

--- init-elpa.el
+++ init-elpa.el
@@ -2,16 +2,20 @@
 
 ;;; Commentary:
 ;;
-;; Settings related to ELPA/package.el. Lifted in large part from Steve
-;;Purcell's emacs.d configuration, found here:
-;; https://github.com/purcell/emacs.d/blob/master/init-elpa.el
+;; Settings related to ELPA/package.el.  Lifted in large part from Steve Purcell's emacs.d
+;; configuration, found here:
+;; <https://github.com/purcell/emacs.d/blob/master/init-elpa.el>
 
 ;;; Code:
 
-;; Find and load the correct package.el. When switching between Emacs 23 and
+;;;
+;;; package.el
+;;;
+
+;; Find and load the correct `package.el'. When switching between Emacs 23 and
 ;; 24, we always use the bundled package.el in Emacs 24.
 (let ((package-el-site-lisp-dir
-       (expand-file-name "site-lisp/package" user-emacs-directory)))
+       (expand-file-name "package" site-lisp-dir)))
   (when (and (file-directory-p package-el-site-lisp-dir)
              (> emacs-major-version 23))
     (message "Removing local package.el from load-path to avoid shadowing bundled version")
@@ -19,40 +23,25 @@
 
 (require 'package)
 
-;; Add support to package.el for pre-filtering available packages
-(defvar package-filter-function nil
-  "Optional predicate function used to internally filter packages used by package.el.
+;;;
+;;; Package Sources
+;;;
 
-The function is called with the arguments PACKAGE VERSION ARCHIVE, where
-PACKAGE is a symbol, VERSION is a vector as produced by `version-to-list', and
-ARCHIVE is the string name of the package archive.")
-
-(defadvice package--add-to-archive-contents
-  (around filter-packages (package archive) activate)
-  "Add filtering of available packages using `package-filter-function', if non-nil."
-  (when (or (null package-filter-function)
-      (funcall package-filter-function
-         (car package)
-         (funcall (if (fboundp 'package-desc-version)
-          'package--ac-desc-version
-        'package-desc-vers)
-            (cdr package))
-         archive))
-    ad-do-it))
-
-
-;;; Standard package repositories
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
 (when (< emacs-major-version 24)
   (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
 (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
+(add-to-list 'package-archives '("melpa-stable" . "http://melpa-stable.milkbox.net/packages/"))
 
-;; Don't use Melpa versions of some packages
-(setq package-filter-function
-      (lambda (package version archive)
-        (or (not (string-equal archive "melpa"))
-            (not (memq package '(slime))))))
+;; If gpg cannot be found, signature checking will fail, so we
+;; conditionally enable it according to whether gpg is available. We
+;; re-run this check once $PATH has been configured
+(defun ndhoule/package-maybe-enable-signatures ()
+  (setq package-check-signature (when (executable-find "gpg") 'allow-unsigned)))
+
+(ndhoule/package-maybe-enable-signatures)
+(eval-after-load 'init-exec-path
+  (ndhoule/package-maybe-enable-signatures))
 
 ;; On-demand installation of packages
 (defun require-package (package &optional min-version no-refresh)
@@ -67,9 +56,13 @@ re-downloaded in order to locate PACKAGE."
         (package-refresh-contents)
         (require-package package min-version t)))))
 
-;; Fire up package.el
+;; Start package.el
+(setq package-enable-at-startup nil)
 (package-initialize)
 
-(provide 'init-elpa)
+(require-package 'fullframe)
+(fullframe list-packages quit-window)
 
+
+(provide 'init-elpa)
 ;;; init-elpa.el ends here
