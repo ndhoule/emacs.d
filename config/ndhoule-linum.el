68106351816e996e3a504f1e693c2c5da12e4231
Nathan Houle <nathan@nathanhoule.com>
2015-04-17 13:37:40 -0700

    Make linum not so ghetto

--- config/ndhoule-linum.el
+++ config/ndhoule-linum.el
@@ -12,43 +12,26 @@
 
 ;; TODO: Is this package still necessary?
 (require-package 'linum-relative)
+;; TODO: Why do we need this?
+(require 'linum-relative)
 
 ;;;;;;;;;;;;;;;;;;;;;
 ;;; Configuration ;;;
 ;;;;;;;;;;;;;;;;;;;;;
 
+;; Enable line numbering in all buffers.
 (global-linum-mode t)
 
-;; Show the column/line number in the modeline
+;; Show the column/line number in the modeline.
 (setq column-number-mode t)
 
-(defvar ndhoule/linum-format-string "%3d ")
-(defvar ndhoule/linum-current-line-number 0)
+;; Format linum column.
+(setq linum-format " %3s  ")
+(setq linum-relative-format " %3s  ")
+(setq linum-relative-current-symbol "")
 
-;; Get line numbers and format the gutter string
-(defun ndhoule/linum-get-format-string ()
-  (let* ((width (1+ (length (number-to-string
-                             (count-lines (point-min) (point-max))))))
-         (format (concat "%" (number-to-string (- width 1)) "d ")))
-    (setq ndhoule/linum-format-string format)))
-
-(defun ndhoule/linum-relative-line-numbers (line-number)
-  (let ((offset (abs (- line-number ndhoule/linum-current-line-number))))
-    (propertize (format ndhoule/linum-format-string offset) 'face 'linum)))
-
-(setq linum-format 'ndhoule/linum-relative-line-numbers)
-
-;;;;;;;;;;;;;
-;;; Hooks ;;;
-;;;;;;;;;;;;;
-
-(add-hook 'linum-before-numbering-hook 'ndhoule/linum-get-format-string)
-
-;; Update line numbers as the cursor moves
-(defadvice linum-update (around my-linum-update)
-  (let ((ndhoule/linum-current-line-number (line-number-at-pos)))
-    ad-do-it))
-(ad-activate 'linum-update)
+;; Enable relative line numbering.
+(linum-relative-toggle)
 
 ;;;;;;;;;;;;;;;;;;;
 ;;; Keybindings ;;;
