9026d5c9af917e1df7c2fa7d2a9699d7c04d6521
Nathan Houle <nathan@nathanhoule.com>
2016-10-20 20:23:29 -0700

    lang-go: Fix messed-up config

--- config/language/ndhoule-lang-go.el
+++ config/language/ndhoule-lang-go.el
@@ -8,43 +8,37 @@
 ;;
 ;;; Code:
 
-(use-package company-go
-  :ensure t
-  :init
-  (with-eval-after-load "company"
-    (add-to-list 'company-backends 'company-go)))
+;; TODO: Do I actually need this now that I'm using ycmd?
+;(use-package company-go
+;  :ensure t
+;  :init
+;  (with-eval-after-load "company"
+;    (add-to-list 'company-backends 'company-go)))
 
+;; TODO: What's this for? :P
 (use-package go-eldoc
   :ensure t
   :init
   (add-hook 'go-mode-hook 'go-eldoc-setup))
 
-(defun ndhoule/init-go ()
-  "Preconfigure Go mode. This runs before the package is required."
-  (add-hook 'before-save-hook #'gofmt-before-save)
-  (setq-default gofmt-command "goimports")
-  (setq tab-width 8)
-  (setq indent-tabs-mode 1)
-  ;; FIXME: Evil mode should use tabs here
-  (add-hook 'go-hook-mode (lambda() (setq indent-tabs-mode t))))
-
-(defun ndhoule/config-go ()
-  "Configure Go mode. This runs after the package is required."
-  (with-eval-after-load "evil"
-    (evil-leader/set-key
-      "mgs" 'godef-jump-other-window
-      "mgd" 'godoc-at-point
-      "mig" 'go-goto-imports
-      "mia" 'go-import-add
-      "mir" 'go-remove-unused-imports)))
-
 (use-package go-mode
   :ensure t
   :mode "\\.go\\'"
   :init
-  (ndhoule/init-go)
-  :config
-  (ndhoule/config-go))
+  (setq-default gofmt-command "goimports")
+
+  (defun ndhoule/go-mode-hook()
+    (add-hook 'before-save-hook 'gofmt-before-save)
+    (with-eval-after-load "evil"
+      (define-key evil-normal-state-local-map (kbd "K") 'godoc-at-point)
+      (evil-leader/set-key
+        "mgs" 'godef-jump-other-window
+        "mgd" 'godoc-at-point
+        "mig" 'go-goto-imports
+        "mia" 'go-import-add
+        "mir" 'go-remove-unused-imports)))
+
+  (add-hook 'go-mode-hook 'ndhoule/go-mode-hook))
 
 (provide 'ndhoule-lang-go)
 ;;; ndhoule-lang-go.el ends here
