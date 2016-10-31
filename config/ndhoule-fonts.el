4bd852477d133b8e7ba8ee3ec90c11d76f070584
Nathan Houle <nathan@nathanhoule.com>
2016-10-30 18:40:17 -0700

    Fix font face in GUI mode

--- config/ndhoule-fonts.el
+++ config/ndhoule-fonts.el
@@ -11,12 +11,17 @@
 ;;;;;;;;;;;;;;;;;;;;;
 
 ;; Set default font
-(set-face-attribute 'default nil
+(set-face-attribute 'default t
                     :family "DejaVu Sans Mono"
                     :height 120
                     :weight 'normal)
 
-;; Line spacing crowds in non-terminal Emacs, expand spacing between lines
+;; When emacs is started framelessly (e.g. as a server), set-face-attribute is
+;; not run. Set default font in that case.
+(add-to-list 'default-frame-alist
+            '(font . "DejaVu Sans Mono for Powerline-12"))
+
+;; Line spacing crowds in GUI; expand spacing between lines
 (setq-default line-spacing 2)
 
 ;;;;;;;;;;;;;;
