8f2139219d709db8cbbcd24d51facf70faefa3a8
Nathan Houle <nathan@nathanhoule.com>
2016-10-30 18:24:44 -0700

    Change company mode trigger settings

--- config/ndhoule-completion.el
+++ config/ndhoule-completion.el
@@ -14,7 +14,7 @@
   ; TODO: Don't point at vim config, point at global installation location
   ; instead. Also, definitely expand this path using $HOME
   (set-variable 'ycmd-server-command
-                '("python2" "/Users/ndhoule/.config/nvim/vendor/YouCompleteMe/third_party/ycmd/ycmd"))
+                '("/usr/local/bin/python2" "/Users/ndhoule/.config/nvim/vendor/YouCompleteMe/third_party/ycmd/ycmd"))
   (add-hook 'after-init-hook #'global-ycmd-mode))
 
 (use-package company-ycmd
@@ -38,25 +38,16 @@
   (define-key company-active-map (kbd "C-k") 'company-select-previous)
   (define-key company-active-map (kbd "C-l") 'company-complete-selection)
 
-
   (setq company-frontends '(company-pseudo-tooltip-frontend))
-  (setq company-dabbrev-downcase nil)
-  (setq company-dabbrev-ignore-case nil)
-
-
-  ;; Set time before completion bubble pops up
-  (setq company-idle-delay 0.05)
-
-  ;; Complete immediately after `.'
-  (setq company-minimum-prefix-length 0)
-
-  ;; Disable blinking
-  (setq company-echo-delay 0)
-
-  ;; Start autocomplete after typing
+  (setq company-tooltip-limit 15)
+  (setq company-idle-delay 0.10)
+  (setq company-minimum-prefix-length 1)
+  (setq company-echo-delay 0) ; fix blinking
   (setq company-begin-commands '(self-insert-command))
+  (setq company-tooltip-align-annotations t)
 
-  (setq company-tooltip-align-annotations t))
+  (setq company-dabbrev-downcase nil)
+  (setq company-dabbrev-ignore-case nil))
 
 (provide 'ndhoule-completion)
 ;;; ndhoule-completion.el ends here
