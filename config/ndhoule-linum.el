320c61b6375b06b0333046d6c1ea625f892cbfa7
Nathan Houle <nathan@nathanhoule.com>
2016-10-30 18:36:41 -0700

    Simplify linum-mode configuration

--- config/ndhoule-linum.el
+++ config/ndhoule-linum.el
@@ -8,23 +8,12 @@
 
 (use-package linum-relative
   :ensure t
-  :config
-  ;; Enable line numbering in all buffers.
-  (global-linum-mode t)
-
-  ;; Show the column/line number in the modeline.
-  (setq column-number-mode t)
-
-  ;; Format linum column.
-  (setq linum-format " %3s  ")
-  (setq linum-relative-format " %3s  ")
-  (setq linum-relative-current-symbol "")
-
-  ;; Enable relative line numbering.
-  (linum-relative-toggle)
-
-  (with-eval-after-load "evil"
-    (evil-leader/set-key "re" 'linum-relative-toggle)))
+  :defer t
+  :init
+  (setq linum-format "%4s ")
+  (setq linum-relative-format "%4s ")
+  (add-hook 'prog-mode-hook 'linum-relative-mode)
+  (add-hook 'text-mode-hook 'linum-relative-mode))
 
 (provide 'ndhoule-linum)
 ;;; ndhoule-linum.el ends here
