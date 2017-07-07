19ae2dae09536bcb88df84690bc11830578b5d14
Nathan Houle <nathan@nathanhoule.com>
2017-07-06 23:58:14 -0700

    Add Protobuf language config

---
+++ config/language/ndhoule-lang-protobuf.el
@@ -0,0 +1,14 @@
+;;; ndhoule-lang-protobuf.el --- protobuf-related settings
+;;
+;;; Commentary:
+;;
+;; Settings that relate to editing protocol buffer definition files go in here.
+;;
+;;; Code:
+
+(use-package protobuf-mode
+  :ensure t
+  :mode "\\.proto\\'")
+
+(provide 'ndhoule-lang-protobuf)
+;;; ndhoule-lang-protobuf.el ends here
