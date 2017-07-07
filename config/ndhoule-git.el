dda03ec03f5e634fbc6cd0b90a2cb084fbaf5cb3
Nathan Houle <nathan@nathanhoule.com>
2017-07-07 00:35:17 -0700

    Git: Remove unused docs, module

--- config/ndhoule-git.el
+++ config/ndhoule-git.el
@@ -6,32 +6,8 @@
 ;;
 ;;; Code:
 
-;;; gist.el
-;;
-;; To configure gist.el:
-;;
-;; Add the following line to `~/.gitconfig':
-;;
-;; ```
-;; [include]
-;;   path = ~/.gitconfig.private
-;; ```
-;;
-;; Generate a new GitHub access token (https://github.com/settings/tokens/new) and add these lines
-;; to `~/.gitconfig.private':
-;;
-;; ```
-;; [github]
-;;   user = your-username
-;;   password = your-personal-access-token
-;;   oauth-token = your-personal-access-token
-;; ```
-
 (use-package github-browse-file
   :ensure t)
-
-;; (use-package git-commit-mode
-;;   :ensure t)
 
 (use-package gitconfig-mode
   :ensure t)
